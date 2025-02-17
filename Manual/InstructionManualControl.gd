76f4c009487c45c4cb7a87ee078668fa52c6d65a
Noah Threlfall-Holmes <124095430+NoahThrelfall-Holmes@users.noreply.github.com>
2025-02-17 21:23:55 +0000

    Added Texture assets
    Pushing in case I break it

--- Manual/InstructionManualControl.gd
+++ Manual/InstructionManualControl.gd
@@ -4,12 +4,15 @@ extends Node3D
 @onready var rightPage: MeshInstance3D = $Book/Mesh_PageR
 @onready var pivot: Node3D = $Book/Pivot
 
+var T_page_blank = preload("res://Assets/Textures/Pages/blank.png")
+var T_page_cover = preload("res://Assets/Textures/Pages/FrontCover.png")
+var T_page_01 = preload("res://Assets/Textures/Pages/Page_01.png")
+var T_page_02 = preload("res://Assets/Textures/Pages/Page_02.png")
+var T_page_03 = preload("res://Assets/Textures/Pages/Page_03.png")
+var T_page_04 = preload("res://Assets/Textures/Pages/Page_04.png")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var T_page_blank = preload("res://Assets/Textures/Pages/blank.png")
-	var T_page_cover = preload("res://Assets/Textures/Pages/FrontCover.png")
-	
-	
 	var leftPageTextPlane = leftPage.get_child(0)
 	if leftPageTextPlane and leftPageTextPlane is MeshInstance3D:
 		var mat = leftPageTextPlane.get_surface_override_material(0)
