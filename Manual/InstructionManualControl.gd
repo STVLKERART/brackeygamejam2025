3af841d9644fad7d2ac39a3059e44f9e62d08ed7
Noah Threlfall-Holmes <124095430+NoahThrelfall-Holmes@users.noreply.github.com>
2025-02-17 17:30:34 +0000

    Front cover

--- Manual/InstructionManualControl.gd
+++ Manual/InstructionManualControl.gd
@@ -6,17 +6,23 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var new_texture = preload("res://Assets/Textures/Pages/testPag.png")
+	var T_page_blank = preload("res://Assets/Textures/Pages/blank.png")
+	var T_page_cover = preload("res://Assets/Textures/Pages/FrontCover.png")
+	
 	
 	var leftPageTextPlane = leftPage.get_child(0)
 	if leftPageTextPlane and leftPageTextPlane is MeshInstance3D:
-		var mat1 = leftPageTextPlane.get_surface_override_material(0)
+		var mat = leftPageTextPlane.get_surface_override_material(0)
 		
-		if mat1 and mat1 is ShaderMaterial:
-			mat1.set_shader_parameter("tex_frg_2", new_texture)
-			print("yum")
-			print (new_texture)
-			print (leftPageTextPlane)
+		if mat and mat is ShaderMaterial:
+			mat.set_shader_parameter("tex_frg_2", T_page_blank)
+	
+	var rightPageTextPlane = rightPage.get_child(0)
+	if rightPageTextPlane and rightPageTextPlane is MeshInstance3D:
+		var mat = rightPageTextPlane.get_surface_override_material(0)
+		
+		if mat and mat is ShaderMaterial:
+			mat.set_shader_parameter("tex_frg_2", T_page_cover)
 
 
 
