a0379d49239d9bc901f2ce09ccf6c0d54bdb4c0c
Jonny <shrimp.lad@gmail.com>
2025-02-22 21:25:21 +0000

    FAN IS IN BATYBEEE

---
+++ Assets/Objects-Meshes/Fan/FanFull.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+
+public partial class FanFull : Node3D
+{
+	[Export] AnimationPlayer anim;
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Ready()
+	{
+		anim.Play();
+		GD.Print("gad");
+	}
+}
